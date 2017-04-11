870e77f2ba60e93837dc1e9590c4835cb87d760c
fducau <fnd212@nyu.edu>
2017-04-11 19:44:34 -0400

    reload path change

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -1097,7 +1097,7 @@ if __name__ == '__main__':
           dictionary='../data/vocab_and_data_small_europarl_v7_enfr/vocab.en.pkl',
           dictionary_src='../data/vocab_and_data_small_europarl_v7_enfr/vocab.fr.pkl',
           use_dropout=False,
-          reload_='./saved_models/fr-en/baseline/vocab50/epoch8_nbUpd120000_model',
+          reload_='./saved_models/fr-en/adversarial_complete/reload_from_exp2/epoch9_nbUpd150000_model',
           correlation_coeff=0.1,
           clip_c=1.,
           adversarial_mode='complete')
